d9b8360389d359f8cb2c7ae9a48e64cfba3512f3
hminiken <33667037+hminiken@users.noreply.github.com>
2020-11-14 17:02:44 -0800

    Add ability to update user information
    Can edit/update user information, except the profile picture. Update page needs some work to fit in with scheme of rest of website. Still need to add ability to update/edit skills

--- Expert Finder/Expert Finder/experts.js
+++ Expert Finder/Expert Finder/experts.js
@@ -89,8 +89,91 @@ module.exports = function () {
 
 	});
 
+
+
+	// ===================================================
+	router.get('/update/:id', function(req, res){
+	console.log("Attempting to update");
+		var mysql = req.app.get('mysql');
+		var index = [req.params.id];
+		let sqlControls = new sqlC.sqlController(res, mysql);
+		sqlControls.setUpIteration(1, 'update-expert');
+
+		// getCategories(sqlControls);
+		// getSkill(sqlControls, 'skill', 1);
+		// getSkill(sqlControls, 'industry', 2)
+		// getSkill(sqlControls, 'course', 3)
+
+		getExpertByID(sqlControls, index);
+		// getSkillsByID(sqlControls,index);
+		// getCoursesByID(sqlControls, index);
+		// getIndustryByID(sqlControls, index);
+
+	});
+
+    // Router to update artifact
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE Experts SET Experts.FirstName=?, Experts.LastName=?, Experts.ProfileEmail=?, Experts.About=?, " + 
+        		   "Experts.GithubLink=?, Experts.LinkedInLink=?, Experts.TwitterLink=? WHERE Experts.ExpertID=?";
+
+        var inserts = [req.body.FirstName,req.body.LastName, req.body.ProfileEmail, req.body.About, req.body.GithubLink,  req.body.LinkedInLink,  req.body.TwitterLink, req.params.id];
+
+        sql = mysql.pool.query(sql, inserts,function(error, results, fields){
+            if(error){
+               console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+
+
+
+
+
+
+	 //    //Display one artifact used to update artifact   
+  //   router.get('/update/:id', function(req, res){
+  //   	console.log("Attempting to update");
+  //   	res.render('update-expert');
+		// var index = [req.params.id];
+		// let sqlControls = new sqlC.sqlController(res, mysql);
+		// sqlControls.setUpIteration(8, 'profile');
+
+		// getCategories(sqlControls);
+		// getSkill(sqlControls, 'skill', 1);
+		// getSkill(sqlControls, 'industry', 2)
+		// getSkill(sqlControls, 'course', 3)
+
+		// getExpertByID(sqlControls, index);
+		// getSkillsByID(sqlControls,index);
+		// getCoursesByID(sqlControls, index);
+		// getIndustryByID(sqlControls, index);
+		// }
+		
+
+
+  //       // // getWingFilterList(res, mysql, context, complete);
+  //       // getArtifact(res, mysql, context, req.params.id, complete);
+  //       // function complete(){
+  //       //     callbackCount++;
+  //       //     if(callbackCount >= 1){
+  //       //         res.render('update-expert', context);
+  //       //     }
+
+  //       // }
+  //   });
+
+
+
 	// Display information about a specifically selected author
 	router.get('/view/:id', function (req, res) {
+		console.log("EXPERT VIEW ID");
+
 		var mysql = req.app.get('mysql');
 		var index = [req.params.id];
 		let sqlControls = new sqlC.sqlController(res, mysql);
@@ -106,6 +189,9 @@ module.exports = function () {
 		getCoursesByID(sqlControls, index);
 		getIndustryByID(sqlControls, index);
 	});
+
+
+
 
 
 	return router;
